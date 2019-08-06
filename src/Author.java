069a62859aee8d5dfffb7b9f2f5831efe08a1f27
JustinSOrtega <justinsortega@gmail.com>
2019-08-06 12:23:53 -0500

    part 1 redone

--- src/Author.java
+++ src/Author.java
@@ -1,33 +1,37 @@
 /**
  * This source file is subject to the license that is bundled with this package in the file LICENSE.
  */
-
 import java.util.ArrayList;
 import java.util.List;
-
+@SuppressWarnings("deprecation")
 public class Author extends Person {
-    private List<String> books;
+    private List books;
+
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList<String>();
+        books = new ArrayList();
     }
 
     /**
      * @deprecated Use publishedBooks instead
      */
     @Deprecated
+    @SuppressWarnings("unchecked")
     public List<String> getBooks() {
         return books;
     }
+    @SuppressWarnings("unchecked")
+
     public List<String> publishedBooks() {
         return books;
     }
+    @SuppressWarnings("unchecked")
 
     public void addBook(String book) {
         books.add(book);
     }
+
     public String sortName() {
         return String.format("%s, %s", lastName, firstName);
     }
-    ///////
 }
